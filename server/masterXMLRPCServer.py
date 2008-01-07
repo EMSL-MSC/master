7c81c6729319c242a220cb04bc6e7c8542a9f51b
kschmidt <kenneth.schmidt@pnnl.gov>
2008-01-07 17:27:56 +0000

    added xmlrpc server code, moved api to server directory.

---
+++ server/masterXMLRPCServer.py
@@ -0,0 +1,42 @@
+import SimpleXMLRPCServer
+
+
+class MasterServerRequestHandler(SimpleXMLRPCServer.SimpleXMLRPCRequestHandler):
+	def report_error(self,code, message):
+		self.send_response(code)
+		response = 'Client Not Authorized'
+		self.send_header("Content-type", "text/plain")
+		self.send_header("Content-length", str(len(response)))
+		self.end_headers()
+		self.wfile.write(message)
+		# shut down the connection
+		self.wfile.flush()
+		self.connection.shutdown(1)
+
+	def do_POST(self):
+		try:
+			if self.authFunction() == True:
+				SimpleXMLRPCServer.SimpleXMLRPCRequestHandler.do_POST(self)
+			else:
+				self.report_error(403,"Client Not Authorized")
+		except NameError, e:
+			self.report_error(500,"Auth function Not Specified"+str(e))
+
+
+class MasterXMLRPCServer(SimpleXMLRPCServer.SimpleXMLRPCServer):
+	functions = []
+	def __init__(self,addr,authFunction):
+		MasterServerRequestHandler.authFunction = authFunction
+		SimpleXMLRPCServer.SimpleXMLRPCServer.__init__(self,addr,MasterServerRequestHandler)
+		
+	def serve_forever(self):
+		for i in self.functions:
+			self.register_function(i,"master.%s" % i.__name__)
+		self.register_introspection_functions()
+		SimpleXMLRPCServer.SimpleXMLRPCServer.serve_forever(self)
+
+	def _dispatch(self, method, params):
+		return SimpleXMLRPCServer.SimpleXMLRPCServer._dispatch(self,method,params)
+
+def rpc(func):
+	MasterXMLRPCServer.functions.append(func)
