3b359de9372c77bd8613b297afd661b538b9314a
dmlb2000 <david.brown@pnnl.gov>
2008-01-09 23:50:09 +0000

    okay did some more checks and return value checking and optional argument passing

--- server/testXMLRPCServer.py
+++ server/testXMLRPCServer.py
@@ -9,7 +9,7 @@ server = xmlrpclib.Server(server_url);
 
 def try_call(function, args):
 	try:
-		print function(*args)
+		retval = function(*args)
 
 	except xmlrpclib.ProtocolError, inst:
 		print "ProtocolError: - %s" % inst
@@ -21,13 +21,32 @@ def try_call(function, args):
 		print "Unexpected error: %s" % inst
 		raise
 
+	if retval:
+		print "Success %s" % retval
+	else:
+		print "Error retval == None"
+
+	return retval
+
 try_call(server.master.addNode, ["n0"])
 try_call(server.master.addStatus, ["n0", "description"])
 try_call(server.master.addProperty, ["n0", "description"])
 try_call(server.master.addUser, ["AbooDaba", "Yoda"])
 try_call(server.master.updateStatus, ["n0", "bad", "AbooDaba","this job sucks","10 years from now"])
-try_call(server.master.getCurrentStatus, ["n0"])
-try_call(server.master.getStatusHistory, ["n0"])
-try_call(server.master.updateProperty, ["n0", "LogicalThinking", "-1"])
-try_call(server.master.getCurrentProperties, ["n0"])
-try_call(server.master.getPropertyHistory, ["n0"])
+try_call(server.master.updateStatus, ["n0", "bad", "AbooDaba","life is cruel for a tester"])
+try_call(server.master.updateStatus, ["n0", "bad", "AbooDaba"])
+try_call(server.master.getCurrentStatus, [["n0"]])
+try_call(server.master.getCurrentStatus, [["n0", "n1"]])
+try_call(server.master.getCurrentStatus, [["n0", "Id0n73xC157"]])
+try_call(server.master.getStatusHistory, [["n0", "n1"]])
+try_call(server.master.getStatusHistory, [["n0"], False, "now", "infinity"])
+try_call(server.master.getStatusHistory, [["n0"], ("n0"), "Wed Jan  9 15:41:52 PST 2008", "now"])
+try_call(server.master.getStatusHistory, [["n0"], ("booboobear"), "Wed Jan  9 15:41:52 PST 2008", "-infinity"])
+try_call(server.master.updateProperty, [["n0"], "LogicalThinking", "-1"])
+try_call(server.master.updateProperty, [["n0"], "LogicalThinking", "-1", "<insert evil laugh here>", "-1"])
+try_call(server.master.getCurrentProperties, [["n0", "n0"]])
+try_call(server.master.getCurrentProperties, [["n0"], ["LogicalThinking"]])
+try_call(server.master.getPropertyHistory, [["n0", "n0"], False, "10 days ago", "now"])
+try_call(server.master.getPropertyHistory, [["n0", "n1"], False, "now", "10 days ago"])
+try_call(server.master.getPropertyHistory, [["n0", "n1"], ["n0"], "now", "10 days ago"])
+try_call(server.master.getPropertyHistory, [["n0", "n0"], ["n0"]])
