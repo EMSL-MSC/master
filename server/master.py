2627cd88c6131c17714361e88feabd6115637a7c
Evan J. Felix <evan.felix@pnnl.gov>
2008-01-09 01:01:12 +0000

    Api changes

--- server/master.py
+++ server/master.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 """
-Below any parameter requiring a node, status, or property will accept a UniqueID or the symbolic name such as 'eth0' or 'cu3cn34'
+Below any parameter requiring a node, status, user, or property accepts a symbolic name such as 'eth0' or 'cu3cn34'
 """
 
 import masterXMLRPCServer as mxs
 
 @mxs.rpc
 def addNode(name):
-	"""addNode(name) -> name
+	"""addNode(name) -> id
 	
 	Create a new node. 
 	Returns new nodes unique ID or None on Error"""
-	return 1
+	return False
 
 @mxs.rpc
 def addStatus(name,description):
@@ -19,7 +19,7 @@ def addStatus(name,description):
 	
 	Create a new status type, with name and descripition.  
 	Returns new status ID or None on error"""
-	return 1
+	return False
 
 @mxs.rpc
 def addProperty(name,description):
@@ -27,35 +27,47 @@ def addProperty(name,description):
 	
 	Create a new property type, with name and descripition.  
 	Returns new status ID or None on Error"""
-	return 1
+	return False
 
 @mxs.rpc
-def updateStatus(nodes,status,comment="",time="now"):
-	"""updateStatus(nodes,status,comment,time) -> [id,...]
+def addUser(name,fullname):
+    """addUser(name,fullname) -> id
+
+    Add a user in the database.
+    name - unique username, should match unix username such as 'efelix'
+    fullname - Printable Name of the user such as 'Evan J. Felix'
+    """
+    return False
+
+@mxs.rpc
+def updateStatus(nodes,status,user,comment="",time="now"):
+	"""updateStatus(nodes,status,user,comment,time) -> Boolean
 	
 	Change the status of a list of nodes.  
 	Comments and time stamp are optional
-	Returns timestamp of change, or None on error"""
-	return 1
+	Returns True on success
+    """
+	return False
 
 @mxs.rpc
 def getCurrentStatus(nodes):
-	"""getCurrentStatus(nodes) -> [(nodeID,nodeName,statusID,statusName,timeStamp,comment),...]
+	"""getCurrentStatus(nodes) -> {'node1':(nodeName,statusName,timeStamp,user,comment),...}
 	
 	Retrieve the current status of a node or set of nodes
 	The nodes parameter is a list of nodes to get status for
 	"""
-	return 1
+	return False
 
 @mxs.rpc
-def getStatusHistory(nodes,filter=None,startTime=None,endTime=None):
-	"""getStatusHistory(nodes,filter=None,startTime=None,endTime=None) -> {'node1':[(statusID,statusName,timestamp,comment),...],'node2':...}
+def getStatusHistory(nodes,filter=False,startTime='-infinity',endTime='inifinity'):
+	"""getStatusHistory(nodes,filter=false,startTime=-infinity,endTime=inifinity) -> {'node1':[(statusName,timestamp,user,comment),...],'node2':...}
 
-	Retrieve a list of status changes in a time period for a list of nodes
+	Retrieve a ordered list of status changes in a time period for a list of nodes
 
 	filter - set of status id's or status names that are allowed to be returned
+    *Time - Date & Time for start and end of data requested
 	"""
-	return 1
+	return False
 
 @mxs.rpc
 def updateProperty(node,property,propertyValue,comment="",time="now"):
@@ -69,36 +81,39 @@ def updateProperty(node,property,propertyValue,comment="",time="now"):
 		time		  - a timestamp for the change.  Defaults to 'now'
 	
 	Returns timestamp of change, or None on error"""
-	return 1
+	return False
 
 @mxs.rpc
-def getCurrentProperties(nodes,filter=None):
-	"""getCurrentProperties(nodes,filter=None) -> {'node':[(propID,propName,value,timestamp,comment),...],'node2':[...],...}
+def getCurrentProperties(nodes,filter=False):
+	"""getCurrentProperties(nodes,filter=False) -> {'node1':[(propName,value,timestamp,comment),...],'node2':[...],...}
 	
 	Retrieve the current properties of the given node.
-	a filter is a list of id's allowed (1,2,3,"eth0mac")
-	Returns a list of tuples containing (propID,propName,value,timestamp,comment)
+	a filter is a list of id's allowed ("eth0mac","eth1mac")
+	Returns a dictionary of tuples containing (propName,value,timestamp,comment)
 
 	"""
-	return 1
+	return False
 
 @mxs.rpc
-def getPropertyHistory(nodes,filter=None,startTime=None,endTime=None):
-	"""getPropertyHistory(nodes,filter=None,startTime=None,endTime=None) -> {'nodeid1':[(propID,propName,value,timestamp,comment),...],'nodeid2':[...],...}
+def getPropertyHistory(nodes,filter=False,startTime='-infinity',endTime='inifinity'):
+	"""getPropertyHistory(nodes,filter=False,startTime='-infinity',endTime='inifinity') -> {'node1':[(propName,value,timestamp,comment),...],'node2':[...],...}
 	
 	Retrieve all property change logs for a given period.
 	If a time period is not specified returns all records.
 	a filter is a list of id's allowed (1,2,3,"eth0mac")
-	Returns a list of tuples containing (propID,propName,value,timestamp,comment)
+	Returns a dictionary list of tuples containing (propName,value,timestamp,comment)
 	"""
-	return 1
+	return False
 
 def authConnection(rh):
 	return rh.client_address[0] == "127.0.0.1"
 
 def main():
+	"""Function to start up the server and serve the pages till the end of time."""
 	srv = mxs.MasterXMLRPCServer(("127.0.0.1",627), authConnection)
 	srv.serve_forever()
 
 if __name__ == "__main__":
 	main()
+
+
