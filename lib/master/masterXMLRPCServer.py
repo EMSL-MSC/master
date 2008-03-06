61912818259c82a18ffbff77d9bc04ce27d8dd01
Evan J. Felix <evan.felix@pnnl.gov>
2008-03-06 21:21:38 +0000

    Some disruptive code movement. A much better setup script. Redhat start up scripts for sark. A default config file.

---
+++ lib/master/masterXMLRPCServer.py
@@ -0,0 +1,51 @@
+import SimpleXMLRPCServer
+import sys
+import master
+
+
+class MasterServerRequestHandler(SimpleXMLRPCServer.SimpleXMLRPCRequestHandler):
+	def report_error(self,code, message):
+		self.send_response(code)
+		response = 'Client Not Authorized'
+		self.send_header("Content-type", "text/plain")
+		self.send_header("Content-length", str(len(response)))
+		self.end_headers()
+		self.wfile.write(message)
+		# shut down the connection
+		self.wfile.flush()
+		self.connection.shutdown(1)
+
+	def do_POST(self):
+		try:
+			if self.authFunction() == True:
+				SimpleXMLRPCServer.SimpleXMLRPCRequestHandler.do_POST(self)
+			else:
+				self.report_error(403,"Client Not Authorized")
+		except NameError, e:
+			self.report_error(500,"Auth function Not Specified"+str(e))
+
+
+class MasterXMLRPCServer(SimpleXMLRPCServer.SimpleXMLRPCServer):
+	functions = []
+	def __init__(self,addr,authFunction):
+		MasterServerRequestHandler.authFunction = authFunction
+		SimpleXMLRPCServer.SimpleXMLRPCServer.__init__(self,addr,MasterServerRequestHandler,0)
+		
+	def serve_forever(self):
+		for i in self.functions:
+			self.register_function(i,"master.%s" % i.__name__)
+		self.register_introspection_functions()
+		SimpleXMLRPCServer.SimpleXMLRPCServer.serve_forever(self)
+
+	def _dispatch(self, method, params):
+		try:
+			return SimpleXMLRPCServer.SimpleXMLRPCServer._dispatch(self,method,params)
+		except:
+			import traceback
+			etype, value, tb = sys.exc_info()
+			master.debug( "Exception Caught:\n%s%s" % ("".join(traceback.format_tb(tb)), traceback.format_exception_only(etype,value)[0]))
+			raise
+
+def rpc(func):
+	MasterXMLRPCServer.functions.append(func)
+	return func
--- setup.py
+++ setup.py
@@ -1,15 +1,77 @@
 #!/usr/bin/python
 
-from setuptools import setup,find_packages
+#from setuptools import setup,find_packages
+from distutils.core import setup
+from distutils.command.install_scripts import install_scripts
+from distutils.command.build_scripts import build_scripts
+from distutils.command.bdist_rpm import bdist_rpm
 
+class local_install_scripts(install_scripts):
+	
+	def finalize_options(self):
+		self.set_undefined_options('install', ('root', 'install_dir'))
+		install_scripts.finalize_options(self)
+	
+class local_build_scripts(build_scripts):
+	
+	def initialize_options(self):
+		build_scripts.initialize_options(self)
+		
+	def get_source_files(self):
+		l=[]
+		for k,v in self.blds.items():
+			print k,v
+			l += v.scripts
+		return l
+
+	def mkbld(self,thedir):
+		b = build_scripts(self.distribution)
+		b.build_dir = self.build_dir+thedir
+		b.scripts = []
+		b.force = self.force
+		b.executable = self.executable 
+		b.outfiles = None
+		return b
+		
+	def finalize_options(self):
+		build_scripts.finalize_options(self)
+		# build up a dictionary of commands for sub scripting
+		self.blds={}
+		
+		for d,files in self.scripts:
+			if not self.blds.has_key(d):
+				self.blds[d]=self.mkbld(d)
+			self.blds[d].scripts += files
+	
+		#for k,v in self.blds.items():
+		#	print k,'=>',self.blds[k].scripts,self.blds[k].build_dir
+				
+	def run(self):
+		for k,v in self.blds.items():
+			v.run()
+
+class local_bdist_rpm(bdist_rpm):
+	def finalize_options(self):
+		if not self.post_install:
+			self.post_install = 'misc/redhat_post_install'
+		self.fix_python = True
+		bdist_rpm.finalize_options(self)
+			
 setup(name='master', version='0.1',
 	author="Evan Felix",
 	author_email="e@pnl.gov",
 	description="""A asset managment system, designed to watch the cluster and also manage its state""",
+	url="https://cvs.pnl.gov/mscf/wiki/MASTER",
 	package_dir= {'master':'lib/master'},
 	packages=['master'],
-	scripts = ['client/master','client/sark'],
-	data_files = [ ("/etc/init.d",["client/master-sark"]),("/usr/sbin",["server/mcp"]) ],
-	requires=['python (>=2.4)']
+	#defautl to default exec_prefix or somesuch.. /usr/bin/ in some cases
+	scripts = [('/usr/bin/',['client/master','client/sark']), ("/etc/init.d",["client/master-sark"]),("/usr/sbin",["server/mcp"])],
+	data_files = [('/etc/',['misc/mcp.conf'])],
+	requires=['python (>=2.4)'],
+	cmdclass = { 
+	    'install_scripts': local_install_scripts, 
+	    'build_scripts': local_build_scripts, 
+	    'bdist_rpm':local_bdist_rpm
+	}
 )
 		
